d68679d7f3390fddd6c3824c5b6bb66ca4133232
BurakOrtakuz <83423289+BurakOrtakuz@users.noreply.github.com>
2022-12-18 17:41:46 +0300

    Güncelleme

---
+++ libft/libft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bortakuz <bortakuz@student.42kocaeli.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/18 17:20:39 by bortakuz          #+#    #+#             */
+/*   Updated: 2022/12/18 17:31:01 by bortakuz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+#endif
